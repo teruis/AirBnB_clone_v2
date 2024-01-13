0c665004658a96165846c4a275bcba790c1796fa
teruis <mohamadelassouli2002@gmail.com>
2024-01-13 12:56:03 +0100

    x

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,25 +6,47 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
+from fabric.exceptions import NetworkError
+
 env.hosts = ['142.44.167.228', '144.217.246.195']
 
-
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        return False
+    """Distributes an archive to the web servers"""
+    if not exists(archive_path):
+        return "Error: Archive not found at {}".format(archive_path)
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
         path = "/data/web_static/releases/"
+
+        # Upload archive to /tmp/
         put(archive_path, '/tmp/')
+
+        # Create directory for new release
         run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
+
+        # Extract contents
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+
+        # Remove the archive
+        run('rm /tmp/{}'.format(file_name))
+
+        # Move contents to the proper location
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
+
+        # Remove the old symbolic link
         run('rm -rf /data/web_static/current')
+
+        # Create a new symbolic link
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
-        return False
+
+        return "Deployment successful!"
+
+    except NetworkError as e:
+        return "NetworkError: {}".format(str(e))
+
+    except Exception as e:
+        return "An error occurred: {}".format(str(e))
+
